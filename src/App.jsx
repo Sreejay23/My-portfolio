77c9939904447f752dc46f0b55a16c6e2de66af0
Sreejay23 <sreejay129@gmail.com>
2026-07-19 13:15:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Projects from './components/Projects'
 import Skills from './components/Skills'
 import About from './components/About'
 import Contact from './components/Contact'
-import Chatbox from './components/Chatbox'
+import ChatBox from './components/ChatBox'
 import './App.css'
 
 function App() {
